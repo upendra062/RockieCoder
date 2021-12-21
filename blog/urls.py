ebb41641e9083013c7e8184857374f2146f53570
Upendra <bhadauriaupendra062@gmail.com>
2021-12-21 17:30:37 +0530

    todays work

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-        path('', views.home, name='home'),
-        path('blog/', views.blog, name='blog'),
+        path('', views.blogHome, name='blogHome'),
+        path('<str:slug>', views.blogPost, name='blogPost'),
         path('rockie/', views.rockie, name='rockie'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def home(index):
-    return HttpResponse('This is home')
+def blogHome(request):
+    return render(request,'blog/blogHome.html')
+    return HttpResponse('This is blogHome. We will keep all the blog posts here')
 
-def blog(index):
-    return HttpResponse('This is blog')
-
-
+def blogPost(request, slug):
+    return render(request, 'blog/blogPost.html')
+    # return HttpResponse(f'This is blogPost: {slug}')
 
 def rockie(index):
     return HttpResponse('This is rockie here')
